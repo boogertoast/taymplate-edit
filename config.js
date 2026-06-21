f1c6d41cb4f7b07c057cd7b2d270d926891dde3e
tdhunded <tdhunded@gmail.com>
2026-06-21 15:36:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -232,13 +232,13 @@ const SITE = {
     review_card_2_bg:     'review2.png',
     review_card_2_avatar: 'https://fairwaylawns.com/wp-content/uploads/2025/12/Ellipse-122.webp',
 
-    review_card_3_bg:     'review3.png',
+    review_card_3_bg:     'review5.png',
     review_card_3_avatar: 'https://fairwaylawns.com/wp-content/uploads/2025/12/Ellipse-122-1.webp',
 
     review_card_4_bg:     'review4.png',
     review_card_4_avatar: 'https://fairwaylawns.com/wp-content/uploads/2025/12/Group-289379-2.webp',
 
-    review_card_5_bg:     'review5.png',
+    review_card_5_bg:     'review3.png',
     review_card_5_avatar: 'https://fairwaylawns.com/wp-content/uploads/2025/12/Judy-Karnes.png',
 
     review_card_6_bg:     'review6.png',
