d6c482be16f115a78d97d7e7307a40e55b6add0c
tdhunded <tdhunded@gmail.com>
2026-07-28 12:50:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -116,7 +116,7 @@ const SITE = {
 
   // Steps
   steps: {
-    heading: "Get A Straight A Driveway in 4 simple steps",
+    heading: "Get A Straight A Driveway in 3 simple steps",
     1: { title: 'Get your free estimate',           body: "Submit the form or call our experts to receive a custom quote based on your needs, no obligation required." },
     2: { title: 'We create your custom plan',        body: 'Our local experts assess your exterior concrete and design a treatment program tailored to you.' },
     3: { title: 'We start scheduled concrete treatment', body: "We show up on schedule to treat your driveway. Your driveway gets washed while you relax. 100% satisfaction guarantee. We're not done until you're happy." },
