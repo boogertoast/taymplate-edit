88115a8326abb3d2997c964f6b6b38150f2a4e4e
tdhunded <tdhunded@gmail.com>
2026-09-10 18:00:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -118,10 +118,10 @@ const SITE = {
   // Steps
   steps: {
     heading: "Get a Straight A Garage Door in 4 Simple Steps",
-    1: { title: 'Get your free estimate',           body: "Submit the form or call our experts to receive a custom quote based on your concrete needs, no obligation required." },
-    2: { title: 'We create your custom plan',        body: 'Our local experts assess your exterior concrete and schedule a treatment program tailored to you.' },
-    3: { title: 'We start concrete treatment', body: "We show up on schedule to treat your concrete."},
-    4: { title: 'Sit back and enjoy a clean concrete',   body: "Your concrete gets washed while you relax. 100% satisfaction guarantee. We're not done until you're happy." },
+    1: { title: 'Get your free estimate',           body: "Submit the form or call our experts to receive a custom quote based on your garage door needs, no obligation required." },
+    2: { title: 'We create your custom plan',        body: 'Our local experts assess your garage door and schedule a repair plan tailored to you.' },
+    3: { title: 'We start your garage door repair', body: "We show up on schedule to repair your garage door."},
+    4: { title: 'Sit back and enjoy a smooth-running garage door',   body: "Your garage door gets repaired while you relax. 100% satisfaction guarantee. We're not done until you're happy." },
   },
 
   // FAQ
